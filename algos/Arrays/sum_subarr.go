cb5fdf9a8f02c87b1e77a936dffca56897e0e41f
Bogdan.Costea <Bogdan.Costea@nuance.com>
2018-10-06 11:19:16 -0400

    subbaray sum solution

--- algos/Arrays/sum_subarr.go
+++ algos/Arrays/sum_subarr.go
@@ -6,11 +6,41 @@ import (
 
 //SubarraySum calculates the number of continuous subarrays that sum to a certain number
 func SubarraySum() {
-	t := []int{1, -1, 1, -1, 1, 1, 2}
+	t := []int{-1, -1, 1}
+	fmt.Printf("%d == 1 \n", goodSolution(t, 0))
 
-	fmt.Println(sSubarraySum(t, 2))
+	t = []int{-1, -1, 1}
+	fmt.Printf("%d == 1 \n", goodSolution(t, 1))
+
+	t = []int{1, -1, 1, -1, 1, 1, 2}
+	fmt.Printf("%d == 4 \n", goodSolution(t, 2))
+
+	t = []int{1, 1, 1}
+	fmt.Printf("%d == 2 \n", goodSolution(t, 2))
+
+	t = []int{1}
+	fmt.Printf("%d == 0 \n", goodSolution(t, 0))
 }
 
+//solution from https://leetcode.com/problems/subarray-sum-equals-k/discuss/102121/C++-prefix-sum-+-map
+//avoid duplicated keys in case of 0 and also doubling because of jumps (no continous arrays as in my solution)
+func goodSolution(nums []int, sum int) int {
+	result := 0
+	sumMap := make(map[int]int)
+	cum := 0
+
+	sumMap[0] = 1
+
+	for _, v := range nums {
+		cum += v
+		result += sumMap[cum-sum]
+		sumMap[cum]++
+	}
+
+	return result
+}
+
+//Not working . Is a bad ideea to put them in a map cause they might get duplicated for certain inputs.
 func sSubarraySum(nums []int, sum int) int {
 	result := 0
 	sumMap := make(map[int]int)
@@ -20,12 +50,20 @@ func sSubarraySum(nums []int, sum int) int {
 	for i, v := range nums {
 		cum += v
 		nums[i] = cum
-		sumMap[sum-cum] = i
+		sumMap[cum-sum]++
 	}
 
+	if c, ok := sumMap[0]; ok {
+		result += c
+	}
 	for _, v := range nums {
-		if _, ok := sumMap[v]; ok {
-			result++
+		if c, ok := sumMap[v]; ok {
+			if sum == 0 {
+				result += c / 2
+				delete(sumMap, v)
+			} else {
+				result += c
+			}
 		}
 	}
 
